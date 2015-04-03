1b97da8874cf8479f718d6b6eea471bb39bc5eed
Brioth <ccelen00@gmail.com>
2015-04-03 21:45:40 +0200

    klaslijst vergeten

---
+++ Groepswerk/Klaslijst.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groepswerk
+{
+    public class Klaslijst : List<string>
+    {
+        public Klaslijst()
+        {
+            {
+                StreamReader bestandKlas = File.OpenText(@"Klassen.txt");
+                string regel = bestandKlas.ReadLine();
+
+                while (regel != null)
+                {
+                    this.Add(regel.Trim());
+                    regel = bestandKlas.ReadLine();
+                }
+                bestandKlas.Close();
+            }
+        }
+
+    }
+}
