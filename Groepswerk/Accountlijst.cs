6a1355f757e4facfce3021f2f843db8973ba9ba2
ThCox <thomascox852@hotmail.com>
2015-04-06 14:18:08 +0200

    GemiddeldesKlas - Thomas

--- Groepswerk/Accountlijst.cs
+++ Groepswerk/Accountlijst.cs
@@ -35,6 +35,6 @@ namespace Groepswerk
             }
             bestandAcc.Close();
         }
-            
+        
     }
 }
---
+++ Groepswerk/GemiddeldesKlas.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Groepswerk
+{
+    /// <summary>
+    /// Interaction logic for GemiddeldesKlas.xaml
+    /// </summary>
+    /* Author: Thomas Cox
+     * Date: 5/04/2015
+    */
+    public partial class GemiddeldesKlas : Window
+    {
+        private Klaslijst klasLijst;
+        private String geselecteerdeKlas;
+        private Gebruiker[] naamGemiddelde;
+
+        public GemiddeldesKlas()
+        {
+            InitializeComponent();
+            klasLijst = new Klaslijst();
+            selecteerKlas.ItemsSource = klasLijst;
+            selecteerKlas.SelectedIndex = 0;
+        }
+
+        private void selecteerKlas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            geselecteerdeKlas=Convert.ToString(selecteerKlas.SelectedItem);
+            
+           // Gebruiker[i]=Accountlijst(string geselecteerdeKlas); 
+           // While loop: Accountlijst zet een boolean naar false zodra de while loop moet stoppen?
+           // While loop: Naam en punten leerling1 in labels zetten
+           // vb: naamLeerling1=Gebruiker[i].getNaam; gemiddeldeW1=Gebruiker[i].getGemiddeldeWiskunde
+        }
+
+
+    }
+}
