7be68861540d4a7f4cf5b11837da3ad4a14c2de1
gristal69 <vincent.vandoninck@student.pxl.be>
2015-04-15 13:36:13 +0200

    change

---
+++ Groepswerk/oefnWisk1.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Groepswerk
+{
+    /// <summary>
+    /// Interaction logic for oefnWisk1.xaml
+    /// 
+    /// //author: Vincent Vandoninck
+        //date: 15/04/2015
+    /// </summary>
+    public partial class oefnWisk1 : Page
+    {
+      
+
+        public oefnWisk1()
+        {
+            InitializeComponent();
+        }
+
+        private void sleepLabel1_DragLeave()
+        {
+
+
+        }
+
+        private void verbeterButton_Click(object sender, RoutedEventArgs e)
+        {
+            //gesleepte uitkomst vergelijken met de uitkomst van de bijhorende oefn.
+        }
+
+        private void sleepLabel1_DragLeave(object sender, DragEventArgs e)
+        {
+
+        }
+    }
+}
