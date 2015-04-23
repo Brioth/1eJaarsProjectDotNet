5702d6368c08313e1905f6cf42374b1d224980c3
Brioth <ccelen00@gmail.com>
2015-04-23 20:00:50 +0200

    Onnodige actieve gebruikers er uit gehaald

--- Groepswerk/Programma.xaml.cs
+++ Groepswerk/Programma.xaml.cs
@@ -113,7 +113,7 @@ namespace Groepswerk
         }
         private void MnuIndOv_Click(object sender, RoutedEventArgs e)
         {
-            Gebruikerdetails detailsMenu = new Gebruikerdetails(ActieveGebruiker);
+            Gebruikerdetails detailsMenu = new Gebruikerdetails();
             framePages.Navigate(detailsMenu);
         }
         private void MnuKlasOver_Click(object sender, RoutedEventArgs e)
@@ -128,7 +128,7 @@ namespace Groepswerk
         }
         private void MnuAccBewerk_Click(object sender, RoutedEventArgs e)
         {
-            AccountBeheer accountBeheerMenu = new AccountBeheer(ActieveGebruiker);
+            AccountBeheer accountBeheerMenu = new AccountBeheer();
             framePages.Navigate(accountBeheerMenu);
         }
         private void MnuKlaslijstBewerk_Click(object sender, RoutedEventArgs e)
