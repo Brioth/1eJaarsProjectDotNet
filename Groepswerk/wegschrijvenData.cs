33b8ef5e3eae79ebfae3ff53c983ad4f45e69a1a
ThCox <thomascox852@hotmail.com>
2015-04-28 22:10:51 +0200

    bestanden toegevoegd aan debug, leg ik morgen wel uit hoe je dat doet

---
+++ Groepswerk/wegschrijvenData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groepswerk
+{
+    class wegschrijvenData
+    {
+        public wegschrijvenData(string sleutelwoord)
+        {
+            switch(sleutelwoord){
+                case "oefNederlandsMakkelijk":
+
+                    break;
+        }
+    }
+    }
+}
