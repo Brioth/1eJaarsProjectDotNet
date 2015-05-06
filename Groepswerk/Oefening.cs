3b6cc3abd578435ac8fdd5ac5d59c73982664c31
ThCox <thomascox852@hotmail.com>
2015-05-06 23:11:54 +0200

    gewoon een titel veranderen jezus

--- Groepswerk/Oefening.cs
+++ Groepswerk/Oefening.cs
@@ -44,9 +44,6 @@ namespace Groepswerk
                 case "taal2":
                     return (opgave + ";" + correcteOplossing + ";" + juisteAntwoordCompleet);
                 break;
-                case default:
-
-                    break;
             }
         }
         public String SchrijfString()
